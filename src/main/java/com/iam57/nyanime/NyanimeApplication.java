5d12282b44c7b66669b725eef1f3963c2921c867
iam57 <me@iam57.com>
2024-03-27 19:49:27 +0800

    refactor(main): 删除多余的@ConfigurationPropertiesScan注解

--- src/main/java/com/iam57/nyanime/NyanimeApplication.java
+++ src/main/java/com/iam57/nyanime/NyanimeApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
-@ConfigurationPropertiesScan("com.iam57.nyanime.properties")
 public class NyanimeApplication {
     public static void main(String[] args) {
         SpringApplication.run(NyanimeApplication.class, args);
